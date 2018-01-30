86ba3c154ce5f2158632ba58f8a302d5f75cad22
realer00 <33505639+realer00@users.noreply.github.com>
2018-01-30 21:31:47 +0800

    Initial commit

---
+++ data.py
@@ -0,0 +1,35 @@
+def Articles():
+	articles=[
+		{
+			'id':1,
+			'title':'article One',
+			'body':'Lorem ipsum dolor sit amet,consescr adikdsak elit, dsae to dsads dsad dd',
+			'author':'Brad Tom',
+			'create_date':'2018-01-28'
+		},
+
+		{
+			'id':2,
+			'title':'article Two',
+			'body':'Lorem ipsum dolor sit amet,consescr adikdsak elit, dsae to dsads dsad dd',
+			'author':'Brad Tom',
+			'create_date':'2018-01-28'
+		},
+
+		{
+			'id':3,
+			'title':'article Three',
+			'body':'Lorem ipsum dolor sit amet,consescr adikdsak elit, dsae to dsads dsad dd',
+			'author':'Brad Tom',
+			'create_date':'2018-01-28'
+		},
+
+		{
+			'id':4,
+			'title':'article four',
+			'body':'Lorem ipsum dolor sit amet,consescr adikdsak elit, dsae to dsads dsad dd',
+			'author':'Brad Tom',
+			'create_date':'2018-01-28'
+		}
+	]
+	return articles
